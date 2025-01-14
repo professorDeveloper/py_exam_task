d664c0a2a1d3a1954036326c5bfbdf24e8a67d18
Azamov X <108933534+professorDeveloper@users.noreply.github.com>
2025-01-14 10:33:04 +0500

    Some bugs fixed

--- main.py
+++ main.py
@@ -33,24 +33,3 @@ while True:
 
 
 
-def prepare_ingredients():
-    print("Sabzavotlarni yuvish, go'shtni kesish.")
-
-
-def cook():
-    print("Pishirish jarayoni: Sabzavotlarni qovurish, go'shtni pishirish.")
-
-
-def garnish():
-    print("Taomni bezash: Yashil giyohlar bilan bezatish.")
-
-
-def make_dish():
-    print("Taom tayyorlashni boshlaymiz:")
-    prepare_ingredients()
-    cook()
-    garnish()
-    print("Taom tayyor!")
-
-
-make_dish()
